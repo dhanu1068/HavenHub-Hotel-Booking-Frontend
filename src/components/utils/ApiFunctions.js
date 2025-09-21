6fad88c390ad1ca6f5fcf3c916a8240278f91463
Dhanunjaya <dhanu1068@gmail.com>
2025-09-21 17:33:06 +0530

    Update ApiFunctions.js

--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 export const api = axios.create({
-	baseURL: "http://localhost:9192"
+	baseURL: API_BASE_URL
 })
 
 export const getHeader = () => {
